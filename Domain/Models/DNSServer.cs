a83ba68c7632f12231f214cc8318aad02236e50d
stvnvc <dstevanovic238@gmail.com>
2024-12-02 15:32:45 +0100

    DNS server

---
+++ Domain/Models/DNSServer.cs
@@ -0,0 +1,40 @@
+﻿using Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class DNSServer
+    {
+        public string IPAdresaServera { get; set; } = string.Empty;
+
+        public int MaksimalanBrojUredjaja = -1;
+
+        public string SimbolickaAresa { get; set; } = string.Empty;
+
+        public TipOperativnogStanja TipOperativnogStanja { get; set; }
+
+        public DNSServer()
+        { }
+
+        public DNSServer(string iPAdresaServera, int maksimalanBrojUredjaja, 
+            string simbolickaAresa, TipOperativnogStanja tipOperativnogStanja)
+        {
+            IPAdresaServera = iPAdresaServera;
+            MaksimalanBrojUredjaja = maksimalanBrojUredjaja;
+            SimbolickaAresa = simbolickaAresa;
+            TipOperativnogStanja = tipOperativnogStanja;
+        }
+
+        public override string? ToString()
+        {
+            return "\nDNS Server: \n" + $"Ip adresa servera: {IPAdresaServera}" +
+                $"\nMaksimalan broj povezanih uredjaja: {MaksimalanBrojUredjaja}" +
+                $"\nSimbolicka adresa: {SimbolickaAresa}" +
+                $"\nTip operativnog stanja: {TipOperativnogStanja}";
+        }
+    }
+}
