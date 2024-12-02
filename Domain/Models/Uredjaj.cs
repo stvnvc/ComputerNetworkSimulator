eb2e1a9eb5caf3288b56f29e57e2cd57a1442568
stvnvc <dstevanovic238@gmail.com>
2024-12-02 15:02:05 +0100

    Uredjaj, Racunar

---
+++ Domain/Models/Uredjaj.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public abstract class Uredjaj
+    {
+        public string SerijskiBrojProizvodjaca { get; set; } = string.Empty;
+
+        protected Uredjaj() { }
+        protected Uredjaj(string serijskiBrojProizvodjaca)
+        {
+            SerijskiBrojProizvodjaca = serijskiBrojProizvodjaca;
+        }
+
+        public override string? ToString()
+        {
+            return $"Serijski broj uredjaja: " + base.ToString();
+        }
+    }
+}
